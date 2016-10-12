d5f095091a60e93af19ed4801c122933d054c08d
Thai An Than Trong <thaianthantrong@dhcp-25-195.invite.emn.fr>
2016-10-12 16:45:57 +0200

    second part of letters

--- ar2rom.py
+++ ar2rom.py
@@ -1,10 +1,11 @@
 import unittest
 
 conversion_map = (
-    ('x', 10),
-    ('v', 5),
-    ('iv', 4),
-    ('i', 1))
+    ('X', 10),
+    ('IX', 9),
+    ('X', 5),
+    ('IV', 4),
+    ('I', 1))
 
 
 def ar2rom(val):
@@ -18,28 +19,31 @@ def ar2rom(val):
 
 class Tests(unittest.TestCase):
     def test_1_to_i(self):
-        self.assertEqual(ar2rom(1), 'i')
+        self.assertEqual(ar2rom(1), 'I')
 
     def test_2_to_ii(self):
-        self.assertEqual(ar2rom(2), 'ii')
+        self.assertEqual(ar2rom(2), 'II')
 
     def test_3_to_iii(self):
-        self.assertEqual(ar2rom(3), 'iii')
+        self.assertEqual(ar2rom(3), 'III')
 
     def test_4_to_iv(self):
-        self.assertEqual(ar2rom(4), 'iv')
+        self.assertEqual(ar2rom(4), 'IV')
 
     def test_5_to_v(self):
-        self.assertEqual(ar2rom(5), 'v')
+        self.assertEqual(ar2rom(5), 'V')
+
+    def test_9_to_ix(self):
+        self.assertEqual(ar2rom(9), 'IX')
 
     def test_10_to_x(self):
-        self.assertEqual(ar2rom(10), 'x')
+        self.assertEqual(ar2rom(10), 'X')
 
     def test_20_to_xx(self):
-        self.assertEqual(ar2rom(20), 'xx')
+        self.assertEqual(ar2rom(20), 'XX')
 
     def test_21_to_xxi(self):
-        self.assertEqual(ar2rom(21), 'xxi')
+        self.assertEqual(ar2rom(21), 'XXI')
 
 if __name__ == "__main__":
     unittest.main()
