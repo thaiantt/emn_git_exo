13e2ce0f36194e964af18ceffc6f3c5f3e95aa95
Thai An Than Trong <thaianthantrong@dhcp-2-183.invite.emn.fr>
2016-10-17 10:42:56 +0200

    add code to convert 50 to L

--- ar2rom.py
+++ ar2rom.py
@@ -1,6 +1,7 @@
 import unittest
 
 conversion_map = (
+    ('L', 50)
     ('x', 10),
     ('ix', 9),
     ('v', 5),
@@ -16,34 +17,37 @@ def ar2rom(val):
             val -= ar
     return res
 
-
 class Tests(unittest.TestCase):
     def test_1_to_i(self):
-        self.assertEqual(ar2rom(1), 'i')
+        self.assertEqual(ar2rom(1), 'I')
 
     def test_2_to_ii(self):
-        self.assertEqual(ar2rom(2), 'ii')
+        self.assertEqual(ar2rom(2), 'II')
 
     def test_3_to_iii(self):
-        self.assertEqual(ar2rom(3), 'iii')
+        self.assertEqual(ar2rom(3), 'III')
 
     def test_4_to_iv(self):
-        self.assertEqual(ar2rom(4), 'iv')
+        self.assertEqual(ar2rom(4), 'IV')
 
     def test_5_to_v(self):
-        self.assertEqual(ar2rom(5), 'v')
+        self.assertEqual(ar2rom(5), 'V')
 
     def test_9_to_ix(self):
         self.assertEqual(ar2rom(9), 'ix')
 
     def test_10_to_x(self):
-        self.assertEqual(ar2rom(10), 'x')
+        self.assertEqual(ar2rom(10), 'X')
 
     def test_20_to_xx(self):
-        self.assertEqual(ar2rom(20), 'xx')
+        self.assertEqual(ar2rom(20), 'XX')
 
     def test_21_to_xxi(self):
-        self.assertEqual(ar2rom(21), 'xxi')
+        self.assertEqual(ar2rom(21), 'XXI')
+    
+    def test_50_to_l(self):
+        self.assertEqual(ar2rom(50), 'L')
+
 
 if __name__ == "__main__":
     unittest.main()
